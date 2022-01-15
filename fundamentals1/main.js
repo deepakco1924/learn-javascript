ce4ecd0704e0fd8b82e1963c0f48c7935dad4f41
Deepak Pal <co19524@ccet.ac.in>
2022-01-15 15:46:13 +0530

    added assignment files

---
+++ fundamentals1/main.js
@@ -0,0 +1,15 @@
+let main = "deepak";
+if (main == "deepak") {
+  //   alert("javscirpt is awesome");
+  console.log("javscirpt is awesome");
+}
+let v = 40 + 4 + 2 + 10 - 20; //making first variable
+// console.log(v);
+// let firstName = "Deepak";
+// console.log(firstName);
+// let last_name = "pal";
+// console.log(last_name);
+// let middle = "sharma";
+// console.log(middle);
+
+//data types
