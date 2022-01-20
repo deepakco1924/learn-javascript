78a49a59e317c791eb501ca020a5003412694b00
Deepak Pal <co19524@ccet.ac.in>
2022-01-20 16:02:10 +0530

    added new chapter dsa

---
+++ data_structures_and_alogrithm/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//from here we learn about the arrays
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+
+const Indianrestaurant = {
+  restaurantName: 'Valcano piazza',
+  location: 'mano vihar,near bandra,mumbai',
+  categories: ['vegetarian', 'non-vegetarian', 'chinease', 'italian', 'franch'],
+  starterMenu: ['panner tikka', 'burger', 'noodles', 'manchurian'],
+  maincourse: ['shai panner', 'rooti', 'daliya', 'chole sbzi'],
+};
+console.log(restaurant);
